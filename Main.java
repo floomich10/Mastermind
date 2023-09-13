174cf676f4f1edfc55f9bfed72c055469675a268
Liam Flood <144454297+floomich10@users.noreply.github.com>
2023-09-13 13:33:01 +0000

    created basic input for number of colours and pegs

--- Main.java
+++ Main.java
@@ -1,10 +1,29 @@
-// import required packages
+import java.util.Scanner;
 
 class Main {
 
   public static void main(String[] args) {
 
-    System.out.println("Hello World");
+    Scanner input = new Scanner(System.in);
+    mastermindGame mastermind = new mastermindGame(0, 0);
+
+    
+    System.out.println("Welcome to Mastermind!");
+
+
+    System.out.println("Enter the number of pegs (1-10): ");
+    mastermind.setPegNumber(input.nextInt());
+
+    System.out.println("Enter the number of colours (1-9): ");
+    mastermind.setPegColours(input.nextInt());
+
+    input.close();
+
+    System.out.println(mastermind.toString());
+    
+
+
+
 
 
   }
