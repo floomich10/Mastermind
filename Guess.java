a2d591fff5c20d0d72d08abb53b66a725d831150
Liam Flood <144454297+floomich10@users.noreply.github.com>
2023-09-13 16:38:12 +0000

    created guess class constrcutor, setter and getter

--- Guess.java
+++ Guess.java
@@ -1,5 +1,24 @@
 
-
 public class Guess {
+
+    private int guesses;
+
+    public Guess(int guesses) {
+
+        this.guesses = guesses;
+    }
+
+    public int getGuesses() {
+
+        return guesses;
+    }
+
+    public void setGuesses(int guesses) {
+
+        this.guesses = guesses;
+    }
+
+
+
 
 }
