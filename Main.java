846fbb9b22b5135b640daf086c1da5ae7cb82e15
Liam Flood <144454297+floomich10@users.noreply.github.com>
2023-09-19 09:36:57 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.Random;
+import java.util.ArrayList;
 
 class Main {
 
@@ -7,6 +9,14 @@ class Main {
     Scanner input = new Scanner(System.in);
     mastermindGame mastermind = new mastermindGame(0, 0);
     Guess guess = new Guess(0);
+  
+    // ArrayList<String> pegs = new ArrayList<String>();
+  
+   // pegs.add();
+    // pegs.add();
+    
+   
+    ArrayList<Peg> answer = new ArrayList<Peg>();
 
     
     System.out.println("Welcome to Mastermind!");
@@ -28,8 +38,25 @@ class Main {
     System.out.println("Since you selected " + mastermind.getPegNumber() + " pegs, and a selection between " + mastermind.getPegColours() + " colours, you will be given " + guess.getGuesses() + " to guess the correct combination.");
 
 
+  // create ANSWER array
+    for (int x = mastermind.getPegNumber(); x > 0; x--) {
+        int randColour = (int)(Math.random()*mastermind.getPegColours()) + 1;
+
+      Peg peg = new Peg(randColour);
+
+      answer.add(peg);
+    }
+
+
+    for (Peg i : answer) {
+      
+      System.out.println(i.getColour());
+    }
     
 
+
+
+      
 
 
 
@@ -37,4 +64,3 @@ class Main {
   }
 
 }
-
---
+++ Peg.java
@@ -0,0 +1,24 @@
+
+public class Peg {
+
+    private int colour;
+
+    public Peg(int colour) {
+
+        this.colour = colour;
+    }
+
+    public int getColour() {
+
+        return colour;
+    }
+
+    public void setColour(int colour) {
+
+        this.colour = colour;
+    }
+
+
+
+
+}
